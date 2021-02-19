eb2b3210543c9e5703c5118ee5af9be45d1a7e70
michealhansun <hansun@live.com>
2021-02-19 23:32:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
+git clone https://github.com/kenzok8/small package/small
+git clone https://github.com/nondanee/UnblockNeteaseMusic package/nondanee
+git clone https://github.com/jerrykuku/luci-theme-argon package/jerrykuku
